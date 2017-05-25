ea142abd45e673593b48581e4a15b7589288ca18
Josmer Hernández <jhernandez.18p@gmail.com>
2017-05-25 02:55:49 -0500

    Allowed

--- settings/settings/base.py
+++ settings/settings/base.py
@@ -18,7 +18,7 @@ if str(DEBUG) == 'True':
     WSGI_APPLICATION = 'settings.wsgi_base.application'
     SITE_URL = '/'
 elif str(DEBUG) == 'False':
-    ALLOWED_HOSTS = ['192.34.61.100','phx.dev2tech.xyz','phoenixworldtrade.com','www.phoenixworldtrade.com']
+    ALLOWED_HOSTS = ['45.55.189.77','174.138.116.15','phx.dev2tech.xyz','phoenixworldtrade.com','www.phoenixworldtrade.com']
     WSGI_APPLICATION = 'settings.wsgi_prod.application'
     SITE_URL = 'http://www.phoenixworldtrade.com'
 

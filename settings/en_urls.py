409ddfcc3e6b535b699d0ea127ed9d8d1905fb65
Josmer Hernández <jhernandez.18p@gmail.com>
2017-05-28 17:52:46 -0500

    url error

--- settings/en_urls.py
+++ settings/en_urls.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import include,url
+from django.conf.urls import (include, url, handler400, handler403, handler404, handler500)
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.static import serve
 
 from local_apps.frontend import views as fw
 from local_apps.profiles import auth as auth_views
+
+handler400 = fw.my_custom_bad_request_view
+handler403 = fw.my_custom_permission_denied_view
+handler404 = fw.my_custom_page_not_found_view
+handler500 = fw.my_custom_error_view
 
 urlpatterns = [
     url(r'^$', fw.index, name="en" ),
